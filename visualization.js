4a0f0b9859166706e34405255e003f276d0ed737
Ram Reddy <69733662+ramnreddy15@users.noreply.github.com>
2020-12-12 15:19:18 -0500

    Update visualization.js

--- visualization.js
+++ visualization.js
@@ -1,7 +1,23 @@
 (function() {
+    var cors_api_host = 'cors-anywhere.herokuapp.com';
+    var cors_api_url = 'https://' + cors_api_host + '/';
+    var slice = [].slice;
+    var origin = window.location.protocol + '//' + window.location.host;
+    var open = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function() {
+        var args = slice.call(arguments);
+        var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+        if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
+            targetOrigin[1] !== cors_api_host) {
+            args[1] = cors_api_url + args[1];
+        }
+        return open.apply(this, args);
+    };
+})();
+(function() {
     console.log("WORKS")
 
-    var margin = { top : 0, left: 0, right: 0, bottom: 0}, height = 400 - margin.top- margin.bottom, width = 800 - margin.left - margin.right;
+    var margin = { top : 0, left: 0, right: 0, bottom: 0}, height = window.innerHeight, width = window.innerWidth;
 
     var svg = d3.select("#map").append("svg")
     .attr("height", height + margin.top + margin.bottom)
@@ -10,19 +26,22 @@
 
     d3.queue().defer(d3.json, "https://alpes.cloud/up/85a75c04c899a39451713622f305ba86.json").await(ready)
 
-    var projection = d3.geoMercator().translate([width/2, height/2]).scale(100)
+    var projection = d3.geoAlbersUsa().translate([width/2, height/2]).scale(window.innerWidth)
 
     var path = d3.geoPath().projection(projection)
+
+    var states
 
     function ready(error, data) {
         console.log(data)
 
-        // var states = topojson.feature(data, data.objects.states).features
-        // console.log(states)
+        states = topojson.feature(data, data.objects.states).features
+        
+        console.log(states)
 
-        // svg.selectAll(".state").data(states).enter().append("path").attr("class", "state")
+        svg.selectAll(".state").data(states).enter().append("path").attr("class", "state").attr("d", path)
+
+
     }
-
-
-
+    
 }) ();
