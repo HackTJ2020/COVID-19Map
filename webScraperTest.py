e4fa747c136d30f36df09d6a2762c83997b23c90
e033005 <ericwfeng@gmail.com>
2020-12-12 12:31:16 -0500

    Update webScraperTest.py

--- webScraperTest.py
+++ webScraperTest.py
@@ -1,6 +1,6 @@
 import urllib.request
 
-target_url = 'https://data.cdc.gov/resource/9mfq-cb36.json?submission_date=2020-12-01T00:00:00.000'
+target_url = 'https://data.cdc.gov/resource/9mfq-cb36.json?submission_date=%272020-1-24T00:00:00.000%27&state=VA'
 data = urllib.request.urlopen(target_url)
 for line in data:
-    print(line)
+    print(decode(line))
