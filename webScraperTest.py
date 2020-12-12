2f71fe33a7153d31f188a423518dd064529b50a0
Ram Reddy <69733662+ramnreddy15@users.noreply.github.com>
2020-12-12 12:32:17 -0500

    Update webScraperTest.py

--- webScraperTest.py
+++ webScraperTest.py
@@ -1,6 +1,9 @@
 import urllib.request
+import numpy as np
 
 target_url = 'https://data.cdc.gov/resource/9mfq-cb36.json?submission_date=2020-12-01T00:00:00.000'
 data = urllib.request.urlopen(target_url)
 for line in data:
     print(line)
+
+
