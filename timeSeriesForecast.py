729a5248338ef4e35be20d806617a68bb2d71314
e033005 <ericwfeng@gmail.com>
2020-12-12 20:04:42 -0500

    Update timeSeriesForecast.py

--- timeSeriesForecast.py
+++ timeSeriesForecast.py
@@ -1,6 +1,7 @@
 import warnings
 import itertools
 import numpy as np
+from pylab import rcParams
 import matplotlib.pyplot as plt
 warnings.filterwarnings("ignore")
 plt.style.use('fivethirtyeight')
@@ -16,7 +17,30 @@ df = pd.read_csv("virginia_covrec.csv")
 cases = df
 cases["Date"] = pd.to_datetime(cases["Date"])
 cases = cases.set_index("Date")
-print(cases.index)
-y=cases["New Cases"].resample('MS').mean()
+#print(cases.index)
+y=cases["New Cases"].resample('M').mean()
 y.plot(figsize=(15,6))
-plt.show()
+#plt.show()
+rcParams['figure.figsize'] = 18, 8
+#print(y)
+decomposition = sm.tsa.seasonal_decompose(y, model='additive', period=5)
+fig = decomposition.plot()
+#plt.savefig("VAdecompose.jpg")
+
+p = d = q = range(0, 2)
+pdq = list(itertools.product(p, d, q))
+seasonal_pdq = [(x[0], x[1], x[2], 12) for x in list(itertools.product(p, d, q))]
+#print('Examples of parameter combinations for Seasonal ARIMA...')
+#print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[1]))
+#print('SARIMAX: {} x {}'.format(pdq[1], seasonal_pdq[2]))
+#print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[3]))
+#print('SARIMAX: {} x {}'.format(pdq[2], seasonal_pdq[4]))
+#finds best parameters that yields best performance:
+for param in pdq:
+    for param_seasonal in seasonal_pdq:
+        try:
+            mod = sm.tsa.statespace.SARIMAX(y,order=param,seasonal_order=param_seasonal,enforce_stationarity=False,enforce_invertibility=False)
+            results = mod.fit()
+            print('ARIMA{}x{}12 - AIC:{}'.format(param, param_seasonal, results.aic))
+        except:
+            continue
