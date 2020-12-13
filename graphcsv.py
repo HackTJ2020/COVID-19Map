07d7d1d90fce5aef342082777e7a3b7a14e7bc23
e033005 <ericwfeng@gmail.com>
2020-12-12 20:51:27 -0500

    Update graphcsv.py

--- graphcsv.py
+++ graphcsv.py
@@ -1,5 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-data = np.genfromtxt("virginia_covrec.csv", delimiter=',')
+data = np.genfromtxt("csvFiles/VA_covrec3.csv", delimiter=',')
 plt.plot(data)
 plt.savefig("VAcovid.jpg")
