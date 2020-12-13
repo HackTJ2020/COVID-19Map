5ae849dc4c930f25a391266a872b9ba11c2458b8
e033005 <ericwfeng@gmail.com>
2020-12-13 12:47:56 -0500

    Update GenerateCSV.py

--- GenerateCSV.py
+++ GenerateCSV.py
@@ -1,17 +1,19 @@
-from CreateCSV import create_CSV
-import numpy as np
-import matplotlib.pyplot as plt
+def generateCSV():
+    from CreateCSV import create_CSV
+    import numpy as np
+    import matplotlib.pyplot as plt
 
-state_names = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA",
-          "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
-          "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
-          "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
-          "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+    state_names = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA",
+            "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+            "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+            "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+            "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
 
 
-for state in state_names:
-    create_CSV(state)
-for state in state_names:
-    data = np.genfromtxt("csvFiles/" + state + "_covrec.csv", delimiter=',')
-    plt.plot(data)
-    plt.savefig("imgFiles/" + state + "covid.jpg")
+    for state in state_names:
+        create_CSV(state)
+    for state in state_names:
+        data = np.genfromtxt("csvFiles/" + state + "_covrec.csv", delimiter=',')
+        plt.plot(data)
+        plt.savefig("Graphs/" + state + "covid.jpg")
+        plt.close()
